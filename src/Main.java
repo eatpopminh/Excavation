1880b29c8a3b15b8be7bb47221a318e5934124dd
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-03-16 19:04:28 -0700

    Messing around

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,10 @@ public class Main {
 		int [][] matrix = new int[num][num];
 		textFileToMatrix2(lines, matrix);
 		
+		//Shitty
 //		int [][] matrix = new int[num+1][num+1];
 //		textFileToMatrix(lines, matrix);
-		//withOutDP();
+//		withOutDP();
 //		withDP(matrix,num);
 		
 
@@ -31,7 +32,6 @@ public class Main {
 	public static int maxSumRectangle(int[][] mat, int num) 
 	{ 
         int preSum[][] = new int[num+1][num+1]; 
-   
         for(int i = 0; i < num; i++) 
         { 
             for(int j = 0; j < num; j++) 
@@ -39,26 +39,27 @@ public class Main {
                 preSum[i+1][j] = preSum[i][j] + mat[i][j]; 
             } 
         } 
-   
+        printMatrix(preSum);
         int maxSum = -1; 
         int minSum = Integer.MIN_VALUE; 
         int negRow = 0, negCol = 0; 
         int rStart = 0, rEnd = 0, cStart = 0, cEnd = 0; 
-        for(int rowStart = 0; rowStart < num; rowStart++) 
+        
+        for(int i = 0; i < num; i++) 
         { 
-            for(int row = rowStart; row < num; row++)
+            for(int j = i; j < num; j++)
             { 
                 int sum = 0; 
                 int curColStart = 0; 
                 for(int col = 0; col < num; col++) 
                 { 
-                    sum += preSum[row+1][col] - preSum[rowStart][col]; 
+                    sum += preSum[j+1][col] - preSum[i][col]; 
                     if(sum < 0) 
                     { 
                         if(minSum < sum) 
                         { 
                             minSum = sum; 
-                            negRow = row; 
+                            negRow = j; 
                             negCol = col; 
                         } 
                         sum = 0; 
@@ -67,8 +68,8 @@ public class Main {
                     else if(maxSum < sum) 
                     { 
                         maxSum = sum; 
-                        rStart = rowStart; 
-                        rEnd = row; 
+                        rStart = i; 
+                        rEnd = j; 
                         cStart = curColStart; 
                         cEnd = col; 
                     } 
@@ -79,10 +80,8 @@ public class Main {
         // Printing final values 
         if(maxSum == -1) 
         { 
-            System.out.println("from row - " + negRow + 
-                                    " to row - " + negRow); 
-            System.out.println("from col - " + negCol +  
-                                " to col - " + negCol); 
+            System.out.println("from row - " + negRow + " to row - " + negRow); 
+            System.out.println("from col - " + negCol + " to col - " + negCol); 
         } 
         else 
         { 
